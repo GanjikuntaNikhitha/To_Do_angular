d86a9f516f4536ca8c79ff5f5b5cf1bc00e50b82
GanjikuntaNikhitha <nikhitha.ganjigunta62@gmail.com>
2020-06-13 16:07:11 +0530

    Creating model for todo app what ever needed by us is added in model

---
+++ to-doapp/src/app/model/Todo.ts
@@ -0,0 +1,8 @@
+// based template how yo like
+export interface Todo {
+    // any thing can repeat but the id will be unique
+    id :string;
+    title : string;
+    isComplete : boolean;
+    date : Date;
+}
